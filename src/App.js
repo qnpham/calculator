023753d04429a9f0fe9400c6dc825ae3b1501257
Quan Pham <mockanterr@gmail.com>
2023-01-14 01:54:19 +0700

    setup basic ui

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="calculator-container">
+        <div className="screen">12301293102</div>
+        <div className="buttons">
+          <span className="column-one-fourth button">7</span>
+          <span className="column-one-fourth button">8</span>
+          <span className="column-one-fourth button">9</span>
+          <span className="column-one-fourth button">/</span>
+          <span className="column-one-fourth button">4</span>
+          <span className="column-one-fourth button">5</span>
+          <span className="column-one-fourth button">6</span>
+          <span className="column-one-fourth button">x</span>
+          <span className="column-one-fourth button">1</span>
+          <span className="column-one-fourth button">2</span>
+          <span className="column-one-fourth button">3</span>
+          <span className="column-one-fourth button">-</span>
+          <span className="column-one-fourth button">0</span>
+          <span className="column-one-fourth button">.</span>
+          <span className="column-one-fourth button">+</span>
+          <span className="column-one-fourth button">=</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
