35749c2a744c58f6141133ae837600d33e3672db
Quan Pham <mockanterr@gmail.com>
2023-01-14 10:56:49 +0700

    finished making number buttons work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [product, setProduct] = useState(0);
+  const [firstNumber, setFirstNumber] = useState(null);
+  const [secondNumber, setSecondNumber] = useState(null);
+  const [onFirstNum, toggleFirstBoolean] = useState(true);
+  function solveHandler() {
+    alert("solved");
+  }
+  useEffect(() => {
+    console.log("first:", firstNumber);
+    console.log("second:", secondNumber);
+  });
+  function buttonHandler(e) {
+    if (e.target.matches("span")) {
+      const number = e.target.innerText;
+      if (onFirstNum) {
+        setFirstNumber(number);
+        toggleFirstBoolean(false);
+      } else {
+        setSecondNumber(number);
+        toggleFirstBoolean(true);
+      }
+    }
+  }
   return (
     <div className="container">
       <div className="calculator-container">
-        <div className="screen">12301293102</div>
-        <div className="buttons">
+        <div className="screen">{product}</div>
+        <div className="buttons" onClick={buttonHandler}>
           <span className="column-one-fourth button">7</span>
           <span className="column-one-fourth button">8</span>
           <span className="column-one-fourth button">9</span>
@@ -21,7 +45,9 @@ function App() {
           <span className="column-one-fourth button">0</span>
           <span className="column-one-fourth button">.</span>
           <span className="column-one-fourth button">+</span>
-          <span className="column-one-fourth button">=</span>
+          <span className="column-one-fourth button" onClick={solveHandler}>
+            =
+          </span>
         </div>
       </div>
     </div>
